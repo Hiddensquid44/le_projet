41a698b0b7964a8f51a178ac4d254964d2389638
nerceau <erceaun44@gmail.com>
2021-06-11 16:22:44 +0200

    TestInfoPorteAnd

--- src/tests/tests.java
+++ src/tests/tests.java
@@ -1,10 +1,17 @@
 package tests;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class tests {
-
-    private static int i;
-
-    public static void main(String[] args) {
-        i=5;
+    @Test
+    public void TestInfoPorteAnd() {
+        Or or = new Or(null, null);
+        Not not = new Not(null);
+        And and = new And(or, not);
+        String desc = and.description();
+        System.out.println(desc);
+        String wanted = "And@" + and.getId() + " in1: " + or.getId() + " in2: " + not.getId();
+        assertEquals(desc, wanted);
     }
 }
