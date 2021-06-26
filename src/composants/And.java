edbc7a4cfbec4f36814874d2fb55cd77bf790b0e
THOMAS <thomas.obledto@gmail.com>
2021-06-26 10:07:58 +0200

    Suivi Commentaire

--- src/composants/And.java
+++ src/composants/And.java
@@ -4,9 +4,10 @@ import signaux.SignalLogique;
 
 
 /*
-La classe And est publique et déscend de Porte2Entrees.
+La classe And est publique et descend de Porte2Entrees.
 
-Il y a 2 types de constructeurs: Un qui permet de créer le composant
+Il y a 2 types de constructeurs: Un qui permet de créer le composant sans paramètre et un qui permet
+de le créer avec avec 2 entrées de type Composant.
 */
 public class And extends Porte2Entrees{
     public And(Composant In1, Composant In2){
@@ -22,6 +23,13 @@ public class And extends Porte2Entrees{
 
     }
 
+    /*
+    La méthode getEtat est publique et renvoie un booleen.
+    Dans le cas où une des 2 entrées n'est pas connectée, elle lance NonConnecteExeption.
+
+    C'est une porte AND, donc si les 2 entrées sont à true, la sortie passe à true.
+    Sinon, elle passe à false.
+    */
     public boolean getEtat() throws NonConnecteException {
         boolean etat;
         if (In1 == null || In2 == null) {
@@ -36,10 +44,20 @@ public class And extends Porte2Entrees{
         return etat;
     }
 
+    /*
+    La méthode getType est publique et renvoie un String.
+
+    Elle renvoie, en chaine de caractère, le type de composant. Ici, "And".
+    */
     public String getType() {
         return "And";
     }
 
+    /*
+    La méthode evaluate est publique et renvoie un SignalLogique.
+
+    NIIIICCOOOLLLAASS
+    */
     @Override
     public SignalLogique evaluate() throws NonConnecteException {
         return In1.evaluate().and(In2.evaluate());
--- src/composants/Composant.java
+++ src/composants/Composant.java
@@ -1,5 +1,14 @@
 package composants;
 
+/*
+La classe Composant est publique, abstraite et NIIIICOOLLLAAASSS
+
+Toutes les classes qui descendent de Composant dovent contenir:
+
+- Une fonction publique getId qui renvoie un String représentant l'ID du composant.
+- Une fonction publique et abstraite description qui renvoie un String représentant l'ID du composant.
+
+*/
 public abstract class Composant implements Comparable <Composant>, Evaluable{
     public String getId(){
         return toString();
